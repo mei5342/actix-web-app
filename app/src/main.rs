9675628133d0ece8fd84776328a034c79b230792
mei5342 <mei13115342@gmail.com>
2022-03-10 23:55:30 +0900

    Streaming response body 作成中

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,19 @@
-use actix_web::{web, App, HttpServer, HttpResponse};
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer};
+use futures::{future::ok, stream::once};
+
+#[get("/stream")]
+async fn stream() -> HttpResponse {
+    let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming(body)
+}
 
 #[actix_web::main]
-async fn main() {
-    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok))).workers(4);
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(stream))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
