2165e2da1f435b3dbc2feea60e4cef84e400f1b6
mei5342 <mei13115342@gmail.com>
2022-03-11 12:58:34 +0900

    作成中

--- app/src/main.rs
+++ app/src/main.rs
@@ -13,7 +13,7 @@ async fn stream() -> HttpResponse {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(stream))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
