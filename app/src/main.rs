be3137f30695a03aa558645976bb6022764868e4
mei5342 <mei13115342@gmail.com>
2022-03-13 17:42:21 +0900

    Either

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,20 +1,17 @@
-use actix_web::{get, web, App, Error, HttpResponse, HttpServer};
-use futures::{future::ok, stream::once};
+use actix_web::{post, web, Either};
+use serde::Deserialize;
 
-#[get("/stream")]
-async fn stream() -> HttpResponse {
-    let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
-
-    HttpResponse::Ok()
-        .content_type("application/json")
-        .streaming(body)
+#[derive(Deserialize)]
+struct Info {
+    name: String,
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(stream))
-        .bind(("wsl2のIPアドレス", 8080))? // wsl2用の設定
-        // 参考：https://rcmdnk.com/blog/2021/03/01/computer-windows-network/
-        .run()
-        .await
+#[post("/")]
+async fn index(form: Either<web::Json<Info>, web::Form<Info>>) -> String {
+    let name: String = match form {
+        Either::Left(json) => json.name.to_owned(),
+        Either::Right(form) => form.name.to_owned(),
+    };
+
+    format!("Welcome {}!", name)
 }
